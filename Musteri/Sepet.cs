04bb4eef23159af7c887da379d49bfb17e4a4922
jeiley <pikac@DESKTOP-G23CHID>
2024-06-23 18:23:49 +0300

    Sepet fonksiyonları eklendi

--- Musteri/Sepet.cs
+++ Musteri/Sepet.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -13,13 +14,17 @@ namespace EnvanterYönetimSistemi.Musteri
 {
     public partial class Sepet : Form
     {
+        SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-G23CHID;Initial Catalog=EnvanterYonetim;Integrated Security=True");
         private int musteriID;
         private List<SepetUrun> sepet;
-        public Sepet(int musteriID,List<SepetUrun> sepet)
+        private MenuMusteri menuMusteri;
+
+        public Sepet(int musteriID, List<SepetUrun> sepet, MenuMusteri menuMusteri)
         {
             InitializeComponent();
             this.musteriID = musteriID;
             this.sepet = sepet;
+            this.menuMusteri = menuMusteri ?? throw new ArgumentNullException(nameof(menuMusteri));
             LoadSepet();
         }
 
@@ -29,7 +34,6 @@ namespace EnvanterYönetimSistemi.Musteri
         }
         private void LoadSepet()
         {
-            dgv_sepet.Rows.Clear();
             foreach (var item in sepet)
             {
                 dgv_sepet.Rows.Add(item.UrunAd, item.Adet, item.ToplamFiyat);
@@ -37,13 +41,77 @@ namespace EnvanterYönetimSistemi.Musteri
         }
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            MenuMusteri menuMusteri = new MenuMusteri(musteriID);
-            menuMusteri.Show();
-            this.Close();
+            if (menuMusteri != null)
+            {
+                menuMusteri.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("MenuMusteri instance is null.");
+            }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void btn_clear_Click(object sender, EventArgs e)
         {
+            if (dgv_sepet.Rows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dgv_sepet.SelectedRows[0];
+                DataRowView dataRowView = (DataRowView)selectedRow.DataBoundItem;
+
+                if (dataRowView != null)
+                {
+                    DataRow dataRow = dataRowView.Row;
+                    dataRow.Delete();
+                    dataRow.Table.AcceptChanges();
+                    MessageBox.Show("Seçilen satır başarıyla silindi.");
+                }
+                else
+                {
+                    dgv_sepet.Rows.RemoveAt(selectedRow.Index);
+                    MessageBox.Show("Seçilen satır başarıyla silindi.");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Lütfen bir satır seçin.");
+            }
+        }
+
+        private void btn_onayla_Click(object sender, EventArgs e)
+        {
+
+            foreach (SepetUrun urun in sepet)
+            {
+                try
+                {
+                    conn.Open();
+                    String durum = "Onay Bekliyor";
+                    String queryToSiparis = $"INSERT INTO Siparis(MusteriID,ToplamTutar,SiparisDurum) VALUES ({musteriID},{urun.ToplamFiyat},'{durum}')";
+                    int siparisID;
+
+                    using (SqlCommand cmd = new SqlCommand(queryToSiparis, conn))
+                    {
+                        siparisID = (int)cmd.ExecuteScalar();
+                    }
+                    String queryToSiparisDetay = $"INSERT INTO SiparisDetay(SiparisID,UrunID,BirimFiyat,Adet) VALUES ({siparisID},{urun.UrunID},{urun.BirimFiyat},{urun.Adet}";
+                    using (SqlCommand cmd = new SqlCommand(queryToSiparisDetay, conn))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Bir hata oluştu: " + ex.Message);
+                }
+                finally
+                {
+                    conn.Close();
+
+                }
+            }
+
 
         }
     }
