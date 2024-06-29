041b689251ad57a0ed5cd1d3ccdff6df20371a05
jeiley <pikac@DESKTOP-G23CHID>
2024-06-29 23:04:36 +0300

    a

--- Calisan/RaporForm.Designer.cs
+++ Calisan/RaporForm.Designer.cs
@@ -28,13 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
             this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ProcessingMode = Microsoft.Reporting.WinForms.ProcessingMode.Remote;
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(800, 450);
+            this.reportViewer1.TabIndex = 0;
             // 
             // RaporForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reportViewer1);
             this.Name = "RaporForm";
             this.Text = "RaporForm";
             this.Load += new System.EventHandler(this.RaporForm_Load);
@@ -43,5 +56,7 @@
         }
 
         #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
     }
 }
--- Calisan/RaporForm.cs
+++ Calisan/RaporForm.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
+using Microsoft.Reporting.WinForms;
 
 namespace EnvanterYönetimSistemi.Calisan
 {
     public partial class RaporForm : Form
     {
+        private string connectionString = @"Data Source=DESKTOP-G23CHID;Initial Catalog=EnvanterYonetim;Integrated Security=True";
+
         public RaporForm()
         {
             InitializeComponent();
@@ -19,7 +17,54 @@ namespace EnvanterYönetimSistemi.Calisan
 
         private void RaporForm_Load(object sender, EventArgs e)
         {
+            LoadStokRapor();
+            LoadSatisRapor();
+            LoadMusteriRapor();
+            this.reportViewer1.RefreshReport();
+        }
 
+        private void LoadStokRapor()
+        {
+            string query = "SELECT UrunAd, StokMiktar, BirimFiyat FROM Urun";
+
+            DataTable dataTable = GetData(query);
+
+            ReportDataSource rds = new ReportDataSource("StokDataSet", dataTable);
+            reportViewer1.LocalReport.DataSources.Add(rds);
+            reportViewer1.LocalReport.ReportPath = "PathToYourStokReport.rdlc";
+        }
+
+        private void LoadSatisRapor()
+        {
+            string query = "SELECT s.SiparisID, m.MusteriAd, s.SiparisTarih, s.ToplamTutar FROM Siparis s JOIN Musteri m ON s.MusteriID = m.MusteriID";
+
+            DataTable dataTable = GetData(query);
+
+            ReportDataSource rds = new ReportDataSource("SatisDataSet", dataTable);
+            reportViewer1.LocalReport.DataSources.Add(rds);
+            reportViewer1.LocalReport.ReportPath = "PathToYourSatisReport.rdlc"; 
+        }
+
+        private void LoadMusteriRapor()
+        {
+            string query = "SELECT MusteriID, MusteriAd, Iletisim, Adres FROM Musteri";
+
+            DataTable dataTable = GetData(query);
+
+            ReportDataSource rds = new ReportDataSource("MusteriDataSet", dataTable);
+            reportViewer1.LocalReport.DataSources.Add(rds);
+            reportViewer1.LocalReport.ReportPath = "PathToYourMusteriReport.rdlc"; 
+        }
+
+        private DataTable GetData(string query)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                return dataTable;
+            }
         }
     }
 }
