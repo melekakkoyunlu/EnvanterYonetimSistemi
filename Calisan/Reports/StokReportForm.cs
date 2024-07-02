57aeda50ad599f2ed3d4db7647e0ab9dc1559e4e
jeiley <pikac@DESKTOP-G23CHID>
2024-07-02 18:03:38 +0300

    aaaaaaaa

--- Calisan/Reports/StokReportForm.cs
+++ Calisan/Reports/StokReportForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Reporting.WinForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -22,22 +23,21 @@ namespace EnvanterYönetimSistemi.Calisan.Reports
         private void StokReportForm_Load(object sender, EventArgs e)
         {
 
+            this.rv_stok.LocalReport.ReportPath = @"C:\Users\pikac\source\repos\EnvanterYönetimSistemi\Calisan\Reports\StokReport.rdlc"; 
             this.rv_stok.RefreshReport();
             SatisRaporLoad();
         }
         private void SatisRaporLoad()
         {
-
-            SqlConnection conn = new SqlConnection(connectionString);
             rv_stok.LocalReport.DataSources.Clear();
-            rv_stok.LocalReport.Refresh();
+            rv_stok.LocalReport.DataSources.Add(new ReportDataSource("StokRaporDataSet"));
             rv_stok.RefreshReport();
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
             RaporForm frm = new RaporForm();
-            frm.ShowDialog();
+            frm.Show();
             this.Close();
         }
     }
